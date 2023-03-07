61fb8d2af0c6a32995bc1bda72cc27b602f1d4e8
GreenSpark404 <test@test.com>
2023-03-07 20:49:00 +0300

    First commit

---
+++ backend/src/main/java/org/greenspark404/QuizApplication.java
@@ -0,0 +1,11 @@
+package org.greenspark404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class QuizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuizApplication.class, args);
+    }
+}
