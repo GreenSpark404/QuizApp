892876675f45189ed3955db76cd18d2ea7705461
sergey_krasnykh <krasnykh@gnivc.ru>
2023-03-14 22:30:45 +0300

    store fixs

--- frontend/src/stores/userStore/userStore.service.ts
+++ frontend/src/stores/userStore/userStore.service.ts
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 const service = {
     loadUserContent(user: object) {
-        return axios.post(`http://localhost:8080/api/auth/login`, user)
-            .then((response) => response.data);
+        return axios.post(`http://localhost:8080/api/auth/login`, user, {
+            withCredentials: true,
+            headers: {'Access-Control-Allow-Origin': 'http://localhost:3000'}
+        })
+            .then((response) => response);
     }
 }
 export default service
