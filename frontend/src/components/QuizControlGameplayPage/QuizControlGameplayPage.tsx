a620f43d3c61ed945fc2fd836590961bcfbd535e
sergey_krasnykh <krasnykh@gnivc.ru>
2023-03-17 22:33:52 +0300

    added websocket

--- frontend/src/components/QuizControlGameplayPage/QuizControlGameplayPage.tsx
+++ frontend/src/components/QuizControlGameplayPage/QuizControlGameplayPage.tsx
@@ -3,14 +3,18 @@ import React, { useEffect } from 'react';
 import classes from './QuizControlGameplayPage.module.scss';
 import Header from '../common/Header';
 import { observer } from 'mobx-react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 type QuizControlGameplayPageProps = {};
 
 const QuizControlGameplayPage: React.FC<QuizControlGameplayPageProps> = ({}) => {
 
-    useEffect(() => {
+    const { sendMessage, lastMessage, readyState } = useWebSocket('wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self');
 
-    }, [])
+    useEffect(() => {
+        console.log(readyState);
+        console.log(lastMessage);
+    }, [lastMessage])
 
   return (
     <div className={classes.component}>
