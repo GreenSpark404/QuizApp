bc07c650b2b747345df1384337efc990932f9200
GreenSpark404 <stereschenkoff@gmail.com>
2023-03-18 20:56:55 +0300

    Add second stub quiz for testing

--- backend/src/main/resources/db/data.sql
+++ backend/src/main/resources/db/data.sql
@@ -1,6 +1,6 @@
 -- Init Stub Quiz
 INSERT INTO QUIZ (ID, DESCRIPTION, NAME)
-VALUES ('stub', 'Stub Quiz', 'Dummy quiz for stubbing purposes');
+VALUES ('stub', 'First Quiz', 'Dummy quiz for stubbing purposes');
 
 INSERT INTO QUIZ_QUESTION_LIST (QUIZ_ID, QUESTION_TEXT, ANSWERS, CORRECT_ANSWER)
 VALUES ('stub',
@@ -22,3 +22,28 @@ VALUES ('stub',
         ARRAY['I', 'II', 'III', 'IV'],
         'III'
        );
+
+INSERT INTO QUIZ (ID, DESCRIPTION, NAME)
+VALUES ('stub2', 'Second Quiz', 'Another dummy quiz for stubbing purposes');
+
+INSERT INTO QUIZ_QUESTION_LIST (QUIZ_ID, QUESTION_TEXT, ANSWERS, CORRECT_ANSWER)
+VALUES ('stub2',
+        'Maecenas pulvinar erat sit amet tempus hendrerit. Vestibulum eget interdum erat, ut convallis risus. Ut ac ultricies lacus. Vestibulum mattis odio id nisl suscipit elementum. Maecenas pretium bibendum erat, vel eleifend justo. Ut quis lorem leo. Maecenas porttitor rutrum purus ac volutpat. Ut quis nisi id lacus mollis varius.',
+        ARRAY['Lorem', 'ipsum', 'dolor', 'sit'],
+        'Lorem'
+       );
+
+INSERT INTO QUIZ_QUESTION_LIST (QUIZ_ID, QUESTION_TEXT, ANSWERS, CORRECT_ANSWER)
+VALUES ('stub2',
+        'Vestibulum facilisis, leo sed molestie convallis, felis neque elementum arcu, sed porta justo quam non quam. Aenean bibendum mattis bibendum. Integer vel vulputate elit. Phasellus cursus ligula et imperdiet tincidunt. Fusce tristique tempus eleifend. Nunc maximus neque id metus sodales maximus. Curabitur a tellus arcu. Maecenas interdum risus ac nibh accumsan, eu aliquam velit suscipit. Cras nibh mauris, tincidunt eget facilisis eu, efficitur a tellus. Vivamus libero eros, gravida at ligula eu, congue dictum lorem.',
+        ARRAY['foo', 'bar', 'abc', 'xyz'],
+        'foo'
+       );
+
+INSERT INTO QUIZ_QUESTION_LIST (QUIZ_ID, QUESTION_TEXT, ANSWERS, CORRECT_ANSWER)
+VALUES ('stub2',
+        'Sed ut consectetur nibh, a vehicula quam. Sed eleifend velit dolor, ut elementum nulla vehicula eu. Phasellus a bibendum est, a iaculis risus. Proin porta, ipsum quis tincidunt iaculis, felis nunc ultrices urna, bibendum pellentesque nisi neque in elit. Pellentesque ut dictum turpis, consectetur volutpat purus. Suspendisse nisi sapien, convallis at accumsan ut, placerat vel velit. Proin a nisl magna. Quisque venenatis blandit maximus.',
+        ARRAY['Alice', 'Bob', 'Eve', 'Chuck'],
+        'Eve'
+       );
+
